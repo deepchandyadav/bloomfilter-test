22c0816fcc0933d68f58d06212f245d74b2c3bbf
Deepchand Yadav <deepchand.yadav@contentstack.com>
2024-09-21 12:51:11 +0530

    db queries

---
+++ db.sql
@@ -0,0 +1,21 @@
+CREATE TABLE Users (
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE
+);;
+
+
+-- create 100 user record
+
+DO $$
+BEGIN
+    FOR i IN 1..100 LOOP
+        INSERT INTO Users (username, email) 
+        VALUES (
+            'user_' || i,
+            'user_' || i || '@example.com'
+        );
+    END LOOP;
+END $$;
+
+select * from users;
