68b0bb1ed457a055f7a613794aec77d3ba24430c
Deepchand Yadav <deepchand.yadav@contentstack.com>
2024-09-24 10:55:21 +0530

    env and db

--- db.sql
+++ db.sql
@@ -1,9 +1,10 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 CREATE TABLE Users (
     id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE
-);;
-
+);
 
 -- create 100 user record
 
