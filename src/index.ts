280770512f11e3fcaa83b1992c4e05aeb08e7ec4
Deepchand Yadav <deepchand.yadav@contentstack.com>
2024-09-26 15:19:15 +0530

    test redis bloom filter

--- src/index.ts
+++ src/index.ts
@@ -10,27 +10,10 @@ dotenv.config({ path: path.resolve(__dirname, "../", ".env") });
 
 const app = new Hono();
 
-
-String.prototype.hashCode = function () {
-  let hash = 0;
-  for (let i = 0; i < this.length; i++) {
-    const char = this.charCodeAt(i);
-    hash = (hash << 5) - hash + char;
-    hash = hash & hash;
-  }
-  return hash;
-}
-
 const redisClient = createClient({
   database: 1,
   url: env("REDIS_URL")
 }).connect();
-
-// const postgresPool = new pg.Pool({
-//   host: env("PG_HOST"),
-//   user: env("PG_USER"),
-//   password: env("PG_PASSWORD")
-// }).connect();
 
 const postgresPool = knex({
   client: "pg",
@@ -41,41 +24,34 @@ const postgresPool = knex({
   }
 })
 
+const BF_KEY = "BF:users";
 
-Promise.all([redisClient, postgresPool]).then(([redis, pg]) => {
+Promise.all([redisClient, postgresPool]).then(async ([redis, pg]) => {
+
+  if (! await redis.exists(BF_KEY)) {
+    console.log("creating Bloom filter key");
+    await redis.bf.reserve(BF_KEY, 0.01, 1000);
+  }
+
 
   const addUser = async (username: string, email: string) => {
-    // const query = `INSERT INTO users (username, email) VALUES ($1, $2) returning *;`;
-    // const user = (await pg.query(query, [username, email])).rows[0];
     const user = pg.insert({ username, email }).into("users");
     await addHashCode(email);
     return user;
   }
 
-  const getBFKey = (data: string) => {
-    const hashCode = data.hashCode().toString();
-    return `bloomfilter:${hashCode.toString()}`;
-  }
-
   const addHashCode = async (email: string) => {
-    const key = getBFKey(email);
-    await redis.set(key, 1);
+    await redis.bf.add(BF_KEY, email);
   }
 
   const addHashCodeMultiple = async (data: Array<Record<string, any>>, keyName: string) => {
-    const rdata: string[] = [];
-    data.forEach(d => {
-      const k: string = d[keyName];
-      rdata.push(getBFKey(k), "1");
-    });
-    await redis.mSet(rdata);
+    const rdata: string[] = data.map(d => d[keyName]);
+    await redis.bf.mAdd(BF_KEY, rdata);
   }
 
   app.get("/add-existing", async (c) => {
-    // const users = (await pg.query("select * from users;")).rows;
     const users = await pg.select().from("users");
     await addHashCodeMultiple(users, "email");
-    // await Promise.all(users.map(u => addHashCode(u.email)));
     return c.text("added");
   })
 
@@ -88,8 +64,7 @@ Promise.all([redisClient, postgresPool]).then(([redis, pg]) => {
       return c.text("Username or email not provided", 400);
     }
 
-    const bfKey = getBFKey(em);
-    const userEx = await redis.exists(bfKey);
+    const userEx = await redis.bf.exists(BF_KEY, em);
 
     if (userEx) {
       return c.text("user exists");
